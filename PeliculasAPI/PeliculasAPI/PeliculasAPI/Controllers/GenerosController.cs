3c549df40d5c019c125c94632fb638fc1c9671f6
josephpatri <jpvillegas2012@gmail.com>
2021-09-06 18:16:57 +0200

    Primer Push API

---
+++ PeliculasAPI/PeliculasAPI/PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/generos")]
+    [ApiController]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GenerosController : ControllerBase
+    {              
+        private readonly ILogger<GenerosController> logger;
+
+        public GenerosController(ILogger<GenerosController> logger)
+        {            
+            this.logger = logger;
+        }
+
+        [HttpGet] // api/generos
+        public ActionResult<List<Genero>> Get()
+        {
+            return new List<Genero>() { new Genero() { Id = 1, Nombre = "Comedia" }};
+        }
+
+        [HttpGet("{Id:int}")]
+        public async Task<ActionResult<Genero>> Get(int Id) => throw new NotImplementedException();
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- PeliculasAPI/PeliculasAPI/PeliculasAPI/Startup.cs
+++ PeliculasAPI/PeliculasAPI/PeliculasAPI/Startup.cs
@@ -1,16 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PeliculasAPI.Filtros;
 
 namespace PeliculasAPI
 {
@@ -26,8 +22,11 @@ namespace PeliculasAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();            
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(FiltroDeExcepcion));
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PeliculasAPI", Version = "v1" });
@@ -37,6 +36,7 @@ namespace PeliculasAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -47,6 +47,8 @@ namespace PeliculasAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
